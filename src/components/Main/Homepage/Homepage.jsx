a2c570ed9b17574049113ee94f429e0ea9854634
194pp <zilvinas111@gmail.com>
2021-12-20 02:27:14 +0200

    Homepage created.

--- src/components/Main/Homepage/Homepage.jsx
+++ src/components/Main/Homepage/Homepage.jsx
@@ -1,9 +1,18 @@
 import classes from './Homepage.module.css';
+import PageHeading from "../../UI/PageHeading";
+import Button from "../../UI/Button";
+import {Link} from "react-router-dom";
 
 const Homepage = () => {
   return (
     <div className={classes.Homepage}>
-      Homepage
+      <PageHeading>Pradžia</PageHeading>
+      <Link to="/login">
+        <Button name="Prisijungti"/>
+      </Link>
+      <Link to="/register">
+        <Button name="Registracija" solid/>
+      </Link>
     </div>
   )
 }
