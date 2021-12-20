f2e5ed8a542eb6eb2838112dcacb3ef707a3133b
194pp <zilvinas111@gmail.com>
2021-12-20 02:24:42 +0200

    Adjusted the Footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import classes from './Footer.module.css';
 const Footer = () => {
   return (
     <footer className={classes.Footer}>
-      Created by Žilvinas Kančys, CodeAcademy final project.
+      Autorius Žilvinas Kančys, CodeAcademy baigiamasis projektas.
     </footer>
   )
 }
