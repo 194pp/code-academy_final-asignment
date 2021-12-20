f4ed253b01dd66e61345c5a2697374451b440917
194pp <zilvinas111@gmail.com>
2021-12-21 00:22:28 +0200

    Added conditional rendering for Homepage buttons depending on whether token is valid or not.

--- src/components/Main/Homepage/Homepage.jsx
+++ src/components/Main/Homepage/Homepage.jsx
@@ -2,17 +2,29 @@ import classes from './Homepage.module.css';
 import PageHeading from "../../UI/PageHeading";
 import Button from "../../UI/Button";
 import {Link} from "react-router-dom";
+import {useAuthContext} from "../../../store/AuthContext";
 
 const Homepage = () => {
+  const {tokenIsValid} = useAuthContext()
   return (
     <div className={classes.Homepage}>
       <PageHeading>Pradžia</PageHeading>
-      <Link to="/login">
-        <Button name="Prisijungti"/>
-      </Link>
-      <Link to="/register">
-        <Button name="Registracija" solid/>
-      </Link>
+      {tokenIsValid ?
+        <>
+          <Link to="/users">
+            <Button name="Vartotojai"/>
+          </Link>
+        </>
+        :
+        <>
+          <Link to="/login">
+            <Button name="Prisijungti"/>
+          </Link>
+          <Link to="/register">
+            <Button name="Registracija" solid/>
+          </Link>
+        </>
+      }
     </div>
   )
 }
--- src/components/Main/Users/Users.jsx
+++ src/components/Main/Users/Users.jsx
@@ -4,17 +4,20 @@ import {useEffect, useState} from "react";
 import {useAuthContext} from "../../../store/AuthContext";
 import UserCardGrid from "./Layout/UserCardGrid";
 import {serverURL} from "../../utils/configs";
+import {useNavigate} from "react-router-dom";
 
 const Users = () => {
+  const navigate = useNavigate();
   const [usersData, setUsersData] = useState([]);
   const {authAxios, tokenBearer} = useAuthContext();
 
   const fetchUsers = async () => {
     await authAxios.get("/users").then((data) => {
-      console.log(data.data.data);
+      // console.log(data.data.data);
       setUsersData(data.data.data.users);
     }).catch((err) => {
-      console.log(err.response);
+      navigate('/');
+      // console.log(err.response);
     });
   }
 
