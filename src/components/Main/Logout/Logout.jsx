c9064639f9446d95fce76c11d2e72e62ac7df4d2
194pp <zilvinas111@gmail.com>
2021-12-20 02:33:23 +0200

    Logout functionality done.

--- src/components/Main/Logout/Logout.jsx
+++ src/components/Main/Logout/Logout.jsx
@@ -1,9 +1,14 @@
 import classes from './Logout.module.css';
+import PageHeading from "../../UI/PageHeading";
+import {useEffect} from "react";
 
 const Logout = () => {
+  useEffect(() => {
+    localStorage.removeItem('vks_key');
+  }, [])
   return (
-    <div>
-      Logged out
+    <div className={classes.Logout}>
+      <PageHeading>Sėkmingai atsijungėte</PageHeading>
     </div>
   )
 }
