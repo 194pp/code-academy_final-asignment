c490979826d035553fc5f46d363c8187e8c27812
194pp <zilvinas111@gmail.com>
2021-12-20 02:23:10 +0200

    Added fetch functions, created configs to store backend API address.

---
+++ src/components/utils/configs.js
@@ -0,0 +1,1 @@
+export const serverURL = process.env.REACT_APP_BACKEND_URL;
---
+++ src/components/utils/fetch.js
@@ -0,0 +1,29 @@
+export async function postFetch(url, body) {
+  try {
+    const resp = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    return await resp.json();
+  } catch (err) {
+    console.log('Post fetch error', err);
+  }
+}
+
+export async function getFetch(url, body) {
+  try {
+    const resp = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    return await resp.json();
+  } catch (err) {
+    console.log('Get fetch error', err);
+  }
+}
