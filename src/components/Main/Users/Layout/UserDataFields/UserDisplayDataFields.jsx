5abc295c3ed963fbd6d5b665e5cbda7a8b4e3cf0
194pp <zilvinas111@gmail.com>
2022-01-06 01:16:04 +0200

    Fixed table error.

--- src/components/Main/Users/Layout/UserDataFields/UserDisplayDataFields.jsx
+++ src/components/Main/Users/Layout/UserDataFields/UserDisplayDataFields.jsx
@@ -18,18 +18,20 @@ const UserDisplayDataFields = ({user}) => {
     <div className={classes.Container}>
       <dl className={classes.Username}>{user.username}</dl>
       <table className={classes.Table}>
-        <tr>
-          <th>Amžius</th>
-          <th>{user.age}</th>
-        </tr>
-        <tr>
-          <th>El. paštas</th>
-          <th>{user.email}</th>
-        </tr>
-        <tr>
-          <th>Sukūrimo data</th>
-          <th>{date}</th>
-        </tr>
+        <tbody>
+          <tr>
+            <th>Amžius</th>
+            <th>{user.age}</th>
+          </tr>
+          <tr>
+            <th>El. paštas</th>
+            <th>{user.email}</th>
+          </tr>
+          <tr>
+            <th>Sukūrimo data</th>
+            <th>{date}</th>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
